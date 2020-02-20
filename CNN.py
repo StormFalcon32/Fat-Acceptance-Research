9d5b30e7357fe3507fafc40f9d314f0524ae0d8c
StormFalcon32 <benchen9708@gmail.com>
2020-02-19 21:20:52 -0500

    Using pytorch instead

--- CNN.py
+++ CNN.py
@@ -1,76 +1,125 @@
-import tensorflow as tf
-from tensorflow import keras
+import torch.optim as optim
 import numpy as np
-import re
+import matplotlib.pyplot as plt
+import torch
+import torchvision
+import torchvision.transforms as transforms
+
+import torch.nn as nn
+import torch.nn.functional as F
 
 
-def decode_review(text):
-    return ' '.join([reverse_word_index.get(i, '?') for i in text])
+def imshow(img):
+    img = img / 2 + 0.5     # unnormalize
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
 
 
-def review_encode(s):
-    encoded = [1]
-    for word in s:
-        if word in word_index:
-            encoded.append(word_index[word])
-        else:
-            encoded.append(2)
-    return encoded
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 5 * 5)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
 
 
-# load data
-imdb = keras.datasets.imdb
-(X_train, y_train), (X_test, y_test) = imdb.load_data(num_words=88000)
-# preprocessing
-word_index = imdb.get_word_index()
+if torch.cuda.is_available():
+    dev = torch.device('cuda')
+    torch.backends.cudnn.benchmark = True
+    torch.backends.cudnn.fastest = True
+else:
+    dev = torch.device('cpu')
 
-word_index = {k: (v+3) for k, v in word_index.items()}
-word_index['<PAD>'] = 0
-word_index['<START>'] = 1
-word_index['<UNK>'] = 2
-word_index['<UNUSED>'] = 3
-reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-train = False
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                        download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
+                                          shuffle=True, num_workers=0)
 
-if train:
+testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                       download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=4,
+                                         shuffle=False, num_workers=0)
 
-    max_len = 250
-    # for review in X_train:
-    #     max_len = max(max_len, len(review))
-    # for review in X_test:
-    #     max_len = max(max_len, len(review))
+classes = ('plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
-    X_train = keras.preprocessing.sequence.pad_sequences(X_train, value=word_index['<PAD>'], padding='post', maxlen=max_len)
-    X_test = keras.preprocessing.sequence.pad_sequences(X_test, value=word_index['<PAD>'], padding='post', maxlen=max_len)
+# net = Net().to(dev)
+# criterion = nn.CrossEntropyLoss()
+# optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
-    # model
-    model = keras.Sequential()
-    model.add(keras.layers.Embedding(88000, 16))
-    model.add(keras.layers.GlobalAveragePooling1D())
-    model.add(keras.layers.Dense(16, activation='relu'))
-    model.add(keras.layers.Dense(1, activation='sigmoid'))
+# for epoch in range(2):  # loop over the dataset multiple times
 
-    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+#     running_loss = 0.0
+#     for i, data in enumerate(trainloader, 0):
+#         # get the inputs; data is a list of [inputs, labels]
+#         inputs, labels = data[0].to(dev), data[1].to(dev)
 
-    X_val = X_train[:3000]
-    X_train = X_train[3000:]
+#         # zero the parameter gradients
+#         optimizer.zero_grad()
 
-    y_val = y_train[:3000]
-    y_train = y_train[3000:]
+#         # forward + backward + optimize
+#         outputs = net(inputs)
+#         loss = criterion(outputs, labels)
+#         loss.backward()
+#         optimizer.step()
 
-    model.fit(X_train, y_train, epochs=40, batch_size=8192, validation_data=(X_val, y_val), verbose=1)
+#         # print statistics
+#         running_loss += loss.item()
+#         if i % 2000 == 1999:    # print every 2000 mini-batches
+#             print('[%d, %5d] loss: %.3f' %
+#                   (epoch + 1, i + 1, running_loss / 2000))
+#             running_loss = 0.0
 
-    # model.save('textNN.h5')
+# print('Finished Training')
+PATH = './cifar_net.pth'
+# torch.save(net.state_dict(), PATH)
+net = Net().to(dev)
+net.load_state_dict(torch.load(PATH))
 
-model = keras.models.load_model(r'D:\Python\textNN.h5')
-with open(r'D:/Python/test.txt', encoding='utf-8') as f:
-    for line in f:
-        nline = re.sub(r'([^\s\w]|_)+', '', line).lower()
-        nline = nline.strip().split(' ')
-        encode = review_encode(nline)
-        encode = keras.preprocessing.sequence.pad_sequences([encode], value=word_index['<PAD>'], padding='post', maxlen=250)  # make the data 250 words long
-        predict = model.predict(encode)
-        print(decode_review(encode[0]))
-        print(encode[0])
-        print(predict[0])
+correct = 0
+total = 0
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data[0].to(dev), data[1].to(dev)
+        outputs = net(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+print('Accuracy of the network on the 10000 test images: %d %%' % (
+    100 * correct / total))
+
+class_correct = list(0. for i in range(10))
+class_total = list(0. for i in range(10))
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data[0].to(dev), data[1].to(dev)
+        outputs = net(images)
+        _, predicted = torch.max(outputs, 1)
+        c = (predicted == labels).squeeze()
+        for i in range(4):
+            label = labels[i]
+            class_correct[label] += c[i].item()
+            class_total[label] += 1
+
+
+for i in range(10):
+    print('Accuracy of %5s : %2d %%' % (
+        classes[i], 100 * class_correct[i] / class_total[i]))
